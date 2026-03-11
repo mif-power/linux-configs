b713c0302050878ddbadfc7d045bed0fa7e3175a
mike <p0we7man@gmail.com>
2026-03-11 10:15:57 +0300

    IG Mosquitto server launch added

--- run_containers.sh
+++ run_containers.sh
@@ -3,6 +3,9 @@
 cd $HOME/soft/infinite_galaxy_bot/control_server/container
 docker compose up -d igbs-laptop
 
+cd /home/mike/soft/infinite_galaxy_bot/mosquitto/
+./start_mqtt.sh
+
 cd $HOME/soft/infinite_galaxy_bot/jupyter_lab/
 docker compose up -d
 
